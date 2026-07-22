d79bd1a87a8b207dcc9d78a3233a1c78e4e8e6a2
betohhh707 <betohhh.ha@gmail.com>
2026-07-22 16:48:36 -0700

    Changed book_id to isbn_13

---
+++ Assignment_2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE WRITER (
+	writer_id INT PRIMARY KEY,
+	writer_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE BOOKS(
+	book_id INT PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	author INT,
+	"publication" INT,
+	page_count INT,
+	FOREIGN KEY (author) REFERENCES WRITER(writer_id)
+);
+
+ALTER TABLE BOOKS RENAME COLUMN book_id TO isbn_13;
